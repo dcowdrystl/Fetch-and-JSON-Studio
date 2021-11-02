6b30e0a772815ad2bb21faa4dfb1071993a2a9c8
Dan Cowdry <medtc411@gmail.com>
2021-11-02 14:51:17 -0700

    added for loop, completed

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 window.addEventListener("load", function() { 
     // let json = [];
+    let container = document.getElementById("container");
+    let astronauts = '';
     fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
         response.json().then(function(json) {
-            console.log(json);
-            const astronauts = document.getElementById("container");
-            
-            container.innerHTML = ` <div class = astronaut>
-                                    <h3> ${json[0].firstName} ${json[0].lastName}</h3> 
-                                    <ul>
-                                    <li>Hours in space: ${json[0].hoursInSpace}</li>
-                                    <li>Active: ${json[0].active}<li>
-                                    <li>Skills: ${json[0].skills}</li>
-                                     </ul>
-                                    <img class = avatar src=${json[0].picture}></img>
-                                    </div>`
-                                    
-                                    
-                                    ;
-
+            // console.log(json);
+            for (let i = 0; i < json.length; i++){
+                astronauts +=  `    <div class="astronaut">
+                                    <div class="bio">
+                                        <h3>${json[i].firstName}${json[i].lastName}</h3>
+                                         <ul>
+                                            <li>Hours in space: ${json[i].hoursInSpace}</li>
+                                            <li>Active: ${json[i].active}</li>
+                                            <li>Skills: ${json[i].skills.join(", ")}</li>
+                                   </ul>
+                                </div>
+                                <img class="avatar" src="${json[i].picture}">
+                             </div>
+                             `;
+            }
+            container.innerHTML = astronauts;
         });
     });
+    
 });
